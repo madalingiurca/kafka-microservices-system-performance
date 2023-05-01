0da91cb73e35712de7ee46a44f52065dcbb6dba3
madalingiurca <mgiurca.mail@gmail.com>
2023-05-01 21:23:33 +0300

    add missing files to vcs

---
+++ mongo/mongo-init.sh
@@ -0,0 +1,27 @@
+set -e
+
+mongosh <<EOF
+use admin
+db.auth("admin", "admin")
+
+use orders
+db.createUser({
+  user: "ordersWorker",
+  pwd: "ordersPass",
+  roles: [ { role: "readWrite", db: "orders" } ]
+})
+
+use payments
+db.createUser({
+  user: "paymentsWorker",
+  pwd: "paymentsPass",
+  roles: [ { role: "readWrite", db: "payments" } ]
+})
+
+use tracking
+db.createUser({
+  user: "trackingWorker",
+  pwd: "trackingPass",
+  roles: [ { role: "readWrite", db: "tracking" } ]
+})
+EOF
